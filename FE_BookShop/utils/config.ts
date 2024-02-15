af7688ac3cdc4447a4c2db01cdae4c094fcb3256
Pemond <vipthieugia200@gmail.com>
2024-02-15 22:13:12 +0700

    done

---
+++ FE_BookShop/utils/config.ts
@@ -0,0 +1,11 @@
+export const homeConfig = {
+  title: 'ReactApp',
+  description: 'Find the best products for the cheapest prices.',
+  keywords: 'ShopBook, cheap ShopBook, buy ShopBook',
+};
+
+export const shippingConfig = {
+  title: 'ReactApp',
+  description: 'Find the best products for the cheapest prices.',
+  keywords: 'ShopBook, cheap ShopBook, buy ShopBook',
+};
